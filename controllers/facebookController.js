35713f571fe59059441d4fa6be0c279551f0435f
joshuaautawi <autawijoshua@gmail.com>
2022-01-24 22:28:43 +0700

    testing

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -48,20 +48,32 @@ const getWebhook = async (req, res) => {
 function handleMessage(sender_psid, received_message) {
   let response;
 
-  // Check if the message contains text
+  // Checks if the message contains text
   if (received_message.text) {
-    // Create the payload for a basic text message
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
     response = {
-      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+      text: `Hi there, What is your name ?`,
     };
   }
-
-  // Sends the response message
   callSendAPI(sender_psid, response);
 }
-
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {}
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
