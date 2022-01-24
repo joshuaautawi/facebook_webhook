b4282b885897b6762c42fee0e81a652a75245ffd
joshuaautawi <autawijoshua@gmail.com>
2022-01-24 20:44:17 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const router = require("./routers/index");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+// api routes
+// app.use('/users', require('./users/users.controller'));
+
+// start server
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 5432;
+app.listen(port, () => console.log("Server listening on port " + port));
